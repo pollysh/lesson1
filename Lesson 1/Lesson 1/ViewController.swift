aab2828605c8c1c83f4a2cbe0588a53f07f49d34
pollysh <84987095+pollysh@users.noreply.github.com>
2021-10-06 20:30:06 +0100

    Add files via upload

---
+++ Lesson 1/Lesson 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Polina Shevchenko on 06/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
